5309f86b489f2c18c1b2778e6cdf522959db2103
David Eje <davideje@dave.local>
2023-03-14 17:08:06 +0300

    In lab submission

---
+++ Lab_07/fake_db.py
@@ -0,0 +1,19 @@
+import psycopg2
+from faker import Faker
+# https://stackabuse.com/working-with-postgresql-in-python/
+con = psycopg2.connect(database="lab_db", user="root",
+                       password="secret", host="127.0.0.1", port="5432")
+
+print("Database opened successfully")
+cur = con.cursor()
+cur.execute('''CREATE TABLE CUSTOMER
+       (ID INT PRIMARY KEY     NOT NULL,
+       Name           TEXT    NOT NULL,
+       Address            TEXT     NOT NULL,
+       review        TEXT);''')
+print("Table created successfully")
+fake = Faker()
+for i in range(1000):
+    print(i)
+    cur.execute("INSERT INTO CUSTOMER (ID,Name,Address,review) VALUES ('"+ str(i)+"','"+fake.name()+"','"+fake.address()+"','"+fake.text()+"')")
+    con.commit()
